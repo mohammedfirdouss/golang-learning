fa66c6bf268a23e575eb7da551c9b037e486b38e
Firdous2307 <mohammedfirdousaraoye@gmail.com>
2024-12-25 14:18:22 +0000

    deleting a key-value pair

--- maps-in-go/main.go
+++ maps-in-go/main.go
@@ -9,12 +9,23 @@ func main() {
     // Add key-value pairs
     myMap["Alice"] = 25
     myMap["Bob"] = 30
+	myMap["Firdous"] = 50
 
     // Access values
-    age, exists := myMap["Bob"]
+    age, exists := myMap["Alice"]
     if exists {
-        fmt.Printf("Bob's age: %d\n", age)
+        fmt.Printf("Alice's age: %d\n", age)
     } else {
         fmt.Println("Key not found")
     }
+
+    // Iterate over the map
+    for key, value := range myMap {
+        fmt.Printf("Key: %s, Value: %d\n", key, value)
+    }
+
+    // Delete a key-value pair
+    delete(myMap, "Bob")
+	delete(myMap, "Firdous")
+    fmt.Println("After deletion:", myMap)
 }
